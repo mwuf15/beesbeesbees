77c2dc44d2cff26a66a8cab21b5e2e0b4237a851
Michael wu <mwuf15@gmail.com>
2020-07-08 11:14:20 -0700

    completed all classes with Pseudoclassical style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
 };
 
+ForagerBee.prototype.eat = function(treasure) {
+  Grub.call(this);
+}
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
+
+};
+RetiredForagerBee.prototype.eat = function() {
+  Grub.call(this);
 };
 
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead"
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure)
+};
